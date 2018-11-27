c051c5c229bbf7112ecfb433f2754a51967e8090
Cidojo <cidell@mail.ru>
2018-11-27 16:53:41 +0300

    добавляю функции подсчета очков, управления жизнями, уровнем и временем состояния, а также тесты к ним

---
+++ js/game-rules.js
@@ -0,0 +1,15 @@
+const GameSetting = {
+  MAX_LEVEL: 10,
+  INITIAL_LIVES: 3,
+  TIME_LIMIT: 30
+};
+
+const ScoreBonus = {
+  CORRECT: 100,
+  QUICK: 50,
+  SLOW: -50,
+  NORMAL: 0,
+  EXTRA_LIFE: 50
+};
+
+export {GameSetting, ScoreBonus};
