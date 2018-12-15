07e6b16ee176760b874d0fe52d67ad29f6f390ac
Cidojo <cidell@mail.ru>
2018-12-15 19:51:47 +0300

    done module4-task2

--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,7 @@
 
 export const getRandomInteger = (max) => Math.floor(Math.random() * max + 1);
 
+
 // @param {markup} html string
 // $return dom node
 
@@ -13,4 +14,25 @@ export const createMarkupNode = (markup) => {
   nestElement.classList.add(`central`);
 
   return nestElement;
+};
+
+
+// @param {frame} object, frame to resize into
+// @param {given} object, given img to resize
+// $result new object with changed img sizes
+
+export const resizeImg = (frame, given) => {
+  const [base, heep] = Object.entries(given)
+    .sort((current, next) => next.pop() - current.pop())
+    .map((it) => it.shift());
+
+  const frameBase = given[base] === given[heep] ?
+    Object.entries(frame)
+      .reduce((current, next) => current.pop() <= next.pop() ? current : next)
+      .pop()
+    : base;
+
+  const multiplier = frame[frameBase] / given[base];
+
+  return {[base]: frame[frameBase], [heep]: given[heep] * multiplier};
 };
